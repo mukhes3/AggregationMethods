36c216603c2f48196fcb882bae12762be5f0a686
Sumit Mukherjee <sumitmukherjee2@gmail.com>
2017-07-17 10:15:31 -0700

    Functiont to calculate distance between clusterings

---
+++ R/GetClusterDistance.R
@@ -0,0 +1,20 @@
+#' Get.Cluster.Distance 
+#' 
+#' This function calculates the distances between two clusterings 
+#' 
+#' @param Clust1 First clustering
+#' @param Clust2 Second clustering
+#' 
+#' 
+
+GetClusterDistance <- function(Clust1, Clust2){ 
+  
+  
+  C1 <- Cluster2Adj(Clust1)
+  C2 <- Cluster2Adj(Clust2)
+  
+  d <- sum(C1==C2)
+  
+  return(d)
+  
+  }
