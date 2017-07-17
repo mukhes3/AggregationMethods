e91864c463ad05a5cde76926d31fbc46adade132
Sumit Mukherjee <sumitmukherjee2@gmail.com>
2017-07-17 11:03:45 -0700

    Adding Best of two
    Compares two consensus clusterings and picks the one with the lowest
    distance to all the clusterings

---
+++ R/BestOfTwo.R
@@ -0,0 +1,32 @@
+#' BestOfTwo 
+#' 
+#' Looks at the resulting clustering from the two algorithms and picks the best 
+#' 
+#'  @param Clust1 Clustering from PickBestCluster
+#'  @param Clust2 Clustering from CC_Pivot 
+#'  @param InstanceList List of all clusterings 
+#'  
+
+ BestOfTwo <- function(Clust1, Clust2, InstanceList){ 
+   
+   l <- length(InstanceList)
+   
+   d1 <- 0 
+   d2 <- 0 
+   
+   for ( i in 1:l){ 
+     
+     d1 <- d1 +  GetClusterDistance(Clust1,InstanceList[[i]])
+     d2 <- d2 +  GetClusterDistance(Clust2,InstanceList[[i]])     
+     
+     }
+   
+   if (d1 <= d2){
+     return(Clust1)
+     
+   } else {
+     
+     return(d2)
+   }
+   
+   }
