09424f4744528124ee42880447a8f7ec46772727
Sumit Mukherjee <sumitmukherjee2@gmail.com>
2017-07-17 15:47:21 -0700

    Updating readme

--- R/ConsensusClusteringExample.R
+++ R/ConsensusClusteringExample.R
@@ -26,22 +26,26 @@ ConsensusClusteringExample <- function(){
   
   par(mfrow=c(2,2))
   
+  #Plotting true clusters based on species
   cat('Plotting true clusters based on species \n')
   plot(iris[,3],iris[,4], col = iris$Species)
   title('Actual clusters')
   
+  #Performing PickBestCluster clustering
   cat('Performing PickBestCluster clustering \n')
   C_1 <- PickBestCluster(InstanceList)
   C_1 <- as.factor(C_1)
   plot(iris[,3],iris[,4], col = C_1)
   title('PickBestCluster')
 
+  #Performing CC-Pivot clustering
   cat('Performing CC-Pivot clustering \n')
   C_2 <- Convert2Labels(CC.Pivot(G),N)
   C_2 <- as.factor(C_2)
   plot(iris[,3],iris[,4], col = C_2)
   title('CC-Pilot clusters')
   
+  #Performing CombinedClusteringWithReps
   cat('Performing CombinedClusteringWithReps, reps = 10 \n')
   C_3 <- CombinedClusteringWithReps(InstanceList, 10)
   C_3 <- as.factor(C_3)
